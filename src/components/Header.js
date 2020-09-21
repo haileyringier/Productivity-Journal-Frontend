025ccb69ab768d2134f6b406956fd87d16621721
haileyringier <haileyringier@outlook.com>
2020-09-21 13:58:28 -0600

    fixes navbar titles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header(){
 
     return(
         <div className='header-section'>
-            <h1 className='welcome-header'>Hello Hailey</h1>
+            {/* <h1 className='welcome-header'>Hello Hailey</h1> */}
             <h2 className='date-header'>September 16, 2020</h2>
         </div>
     )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,10 @@ import TodayOutlinedIcon from '@material-ui/icons/TodayOutlined';
 import AssessmentOutlinedIcon from '@material-ui/icons/AssessmentOutlined';
 import MenuBookOutlinedIcon from '@material-ui/icons/MenuBookOutlined';
 import HomeIcon from '@material-ui/icons/Home';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap'
 
 export default function NavBar(){
 
@@ -11,14 +15,19 @@ const menuOpen = () =>{
     console.log("menu clicked")
 }
     return(
+     
         <div className='nav-bar'>
-        <h1 className='page-title'>Productivity Journal</h1>
+        <h1 className='page-title'>Hello Hailey</h1>
+        <div className='right-nav'>
             <nav className='navbar-links'>
                 <Link className='nav-link' to='/'><HomeIcon/> Home</Link>
                 <Link className='nav-link' to='/calendar'><TodayOutlinedIcon/> Calendar</Link>
                 <Link className='nav-link' to='/habittracker'><AssessmentOutlinedIcon/> Habits</Link>
                 <Link className='nav-link' to='/journal'><MenuBookOutlinedIcon/> Journal</Link>
+                {/* <Link className='nav-link' to='/profile'><AccountCircleIcon /> Profile</Link> */}
+                {/* <button className='sign-out-button'><ExitToAppIcon /> Sign out</button> */}
             </nav>
+        </div>
         </div>
     )
 }
