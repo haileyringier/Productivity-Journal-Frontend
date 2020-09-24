f7af0783f4c1480fc81847944c7de2990bf14200
haileyringier <haileyringier@outlook.com>
2020-09-24 09:43:46 -0600

    adds monthly calendar view

--- src/components/Calendar/CalendarPage.js
+++ src/components/Calendar/CalendarPage.js
@@ -1,15 +1,45 @@
 import React from 'react'
 import Event from './Event'
 
-export default function CalendarPage(props){
+import { Calendar, Views, momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+
+
+const localizer = momentLocalizer(moment)
+
+export default class CalendarPage extends React.Component{
+    
+    state = {
+        events: [
+          {
+            start: moment().toDate(),
+            end: moment()
+              .add(1, "days")
+              .toDate(),
+            title: "Some title"
+          }
+        ]
+    }
 
     // const showEvents = () => props.myEvents.map(event => {
     //     return <Event title={event.title} content={event.content} date={event.date} />
     // })
-    
-    return(
+    render(){
+      return(
         <div>
             <h1>Calendar Page</h1>
+            <Calendar
+                localizer={localizer}
+                events={this.state.events}
+                startAccessor="start"
+                endAccessor="end"
+              defaultView="month"
+              defaultDate={moment().toDate()}
+              style={{ height: "100vh" }}
+            />
         </div>
     )
+}
 }
